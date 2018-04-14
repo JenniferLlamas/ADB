3e69274ca107edbf081170dd8b9102e266621eec
Jennifer Llamas <petra15>
2018-04-14 14:49:07 -0700

    design

---
+++ ABD/ABD/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ABD
+{
+    public partial class Ventana1 : Form
+    {
+        public Ventana1()
+        {
+            InitializeComponent();
+            
+        }
+        Form2 ventana = new Form2();
+        string bd=null;
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ventana.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ventana.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ventana.Show();
+        }
+    }
+}
